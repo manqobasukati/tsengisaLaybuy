8af652187062c323d9568cdaccb6a07db9c8b9df
manqobasukati <manqoba.sukati@peoplesized.com>
2024-05-16 19:28:06 +0200

    yes

--- layouts/default.vue
+++ layouts/default.vue
@@ -25,13 +25,16 @@
         <img
           width="100"
           class="rounded-full"
-          src="https://media.licdn.com/dms/image/C4D03AQGo606g1nklfA/profile-displayphoto-shrink_200_200/0/1580191988102?e=1714608000&v=beta&t=hkBOdQ03McRa5yrm_0mEWgVgxiAv8PKLxar1REPA8J0"
+          src="https://media.licdn.com/dms/image/C4D03AQGo606g1nklfA/profile-displayphoto-shrink_200_200/0/1580191988102?e=1721260800&v=beta&t=Yg9JmKoR9pAsqf7WIJZ2iBKZZSwIR0Yv1TZOKdlqOeA"
         />
         <li v-for="(item, key) in menuItems" class="w-full" :key="key">
-          <div class="flex justify-left w-full">
+          <NuxtLink :href="item.itemLink">
+            <div class="flex justify-left w-full items-center gap-1">
             <span class="material-icons-outlined">{{ item.itemIcon }}</span>
             <div>{{ item.itemName }}</div>
           </div>
+          </NuxtLink>
+        
           <div class="divider"></div>
         </li>
       </ul>
@@ -43,31 +46,43 @@
 import { ref } from 'vue';
 
 import type { Ref } from 'vue';
+import { RouterLink } from 'vue-router';
 type MenuItem = {
   itemName: string;
   itemIcon: string;
+  itemLink?:string
 };
 
 const menuItems: Ref<MenuItem[]> = ref([
   {
     itemName: 'LayBuys',
+    itemLink:"/laybuys?tab=New",
     itemIcon: 'shopping_bag',
   },
   {
     itemName: 'Profile',
+    itemLink:"/laybuys?tab=New",
     itemIcon: 'person',
   },
   {
     itemName: 'FAQ',
+    itemLink:"/laybuys?tab=New",
     itemIcon: 'help',
   },
   {
     itemName: 'Settings',
+    itemLink:"/settings",
     itemIcon: 'settings',
   },
   {
     itemName: 'Privacy Policy',
+    itemLink:"/laybuys?tab=New",
     itemIcon: 'policy',
+  },
+  {
+    itemName: 'Log out',
+    itemLink:"/laybuys?tab=New",
+    itemIcon: 'power_settings_new',
   },
 ]);
 </script>
