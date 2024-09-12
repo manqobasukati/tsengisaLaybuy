d2df9618ecdbdf8ab2dc966c9ba4ed940f1fae35
manqobasukati <manqoba.sukati@peoplesized.com>
2024-09-12 20:05:51 +0200

    vele

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,5 +12,30 @@ export default defineNuxtConfig({
   devServer:{
     host:"0.0.0.0"
   },
- 
+  pwa: {
+    manifest: {
+      name: "Tsengisa PWA",
+      short_name: "pwa nuxt",
+      theme_color:'#8a8a5e',
+      description: "Arman Abi r.man.abi@gmail.com",
+      icons: [
+        {
+          src: 'image.png',
+          sizes: "150x150",
+          type: "image/png"
+        },
+      ]
+
+
+    },
+    workbox: {
+      navigateFallback: "/",
+
+    },
+    devOptions: {
+      enabled: true, 
+      type: "module"
+    }
+
+  }
 })
