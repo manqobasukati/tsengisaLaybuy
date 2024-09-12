d19123d09c1749838406bd710b9aadb080bbf5f8
manqobasukati <manqoba.sukati@peoplesized.com>
2024-09-12 22:19:39 +0200

    give me more

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,7 +17,7 @@ export default defineNuxtConfig({
     manifest: {
   
       name: 'Tsengisa',
-      short_name: 'AppName',
+      short_name: 'Tsengisa',
       description: 'Your App Description',
       theme_color: '#ffffff',  // Customize as needed
       background_color: '#ffffff',  // Customize as needed
@@ -25,7 +25,7 @@ export default defineNuxtConfig({
       start_url: '/',
       icons: [
         {
-          src: '/logo.png',
+          src: 'logo.png',
           sizes: '192x192',
           type: 'image/png'
         },
