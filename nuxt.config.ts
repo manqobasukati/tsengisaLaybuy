8bfb20718c275a45094f022a7c1f0c50fcf4b6c2
manqobasukati <manqoba.sukati@peoplesized.com>
2024-09-12 20:42:44 +0200

    more

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,49 +13,29 @@ export default defineNuxtConfig({
     host: '0.0.0.0',
   },
   pwa: {
-    // manifest: {
+    manifest: {
     //   name: "Tsengisa PWA",
 
     //   short_name: "pwa nuxt",
-    //   theme_color:'#8a8a5e',
-    //   description: "Arman Abi r.man.abi@gmail.com",
-    //   icons: [
-    //     {
-    //       src: 'image.png',
-    //       sizes: "150x150",
-    //       type: "image/png"
-    //     },
-    //   ]
+      theme_color:'#8a8a5e',
+      description: "Arman Abi r.man.abi@gmail.com",
+      icons: [
+        {
+          src: 'image.png',
+          sizes: "150x150",
+          type: "image/png"
+        },
+      ]
 
-    // },
-    // workbox: {
-    //   navigateFallback: "/",
-
-    // },
-    // devOptions: {
-    //   enabled: true,
-    //   type: "module"
-    // }
-    mode: 'development',
-    // disable PWA only when in preview mode
-    scope: '/',
-    // srcDir: './service-worker',
-    // filename: 'sw.ts',
-    strategies: 'injectManifest',
-   // injectRegister: false,
-    includeManifestIcons: false,
-    manifest: {
-      name:"Appy"
     },
-    // injectManifest: {
-    //   globPatterns: [
-    //     '**/*.{js,json,css,html,txt,svg,png,ico,webp,woff,woff2,ttf,eot,otf,wasm}',
-    //   ],
-    //   globIgnores: ['emojis/**', 'manifest**.webmanifest'],
-    // },
+    workbox: {
+      navigateFallback: "/",
+
+    },
     devOptions: {
-      enabled: process.env.VITE_DEV_PWA === 'true',
-      type: 'module',
-    },
-  },
+      enabled: true,
+      type: "module"
+    }
+
+  }
 });
